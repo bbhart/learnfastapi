989176675617cc230895b2d79a35752c8e76b238
Brian Hart <bbhart@bbhart.com>
2024-03-08 18:48:27 -0500

    Working on db

--- project3/main.py
+++ project3/main.py
@@ -1,7 +1,24 @@
-from fastapi import FastAPI
-from database import engine
+from typing import Annotated
+from fastapi import Depends, FastAPI, status
+from database import SessionLocal, engine
 import models
+from models import Todos
+from sqlalchemy.orm import Session
 
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+@app.get("/", status_code=status.HTTP_200_OK)
+async def read_all( db: db_dependency ):
+    return db.query(Todos).all()
+
